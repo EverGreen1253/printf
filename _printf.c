6ba925172cfdb8861dfbd85c42ae3727ae06ce28
Adrian <dreamweaver3d@gmail.com>
2023-08-11 14:44:03 +0800

    printf commit 7

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -21,36 +20,47 @@ int _printf(const char *format, ...)
 	int j = 0;
 	int len = 0;
 	int startpos = 0;
+	int endpos = 0;
 
 	if (!format)
 	{
 		exit(98);
 		return (-1);
 	}
+
 	va_start(list, format);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1])
+		{
+			endpos = i - 1;
 			while (j < 5)
 			{
 				if (*ops[j].t == format[i + 1])
 				{
-					print_substring(format, startpos, (i - 1));
-					len = len + (((i - 1) - startpos) + 1) + ops[j].f(&list);
+					print_substring(format, startpos, endpos);
+					len = len + ((endpos - startpos) + 1) + ops[j].f(&list);
 					startpos = i + 2;
 					i = startpos - 1;
+					j = 5;
 				}
 				j = j + 1;
 			}
+		}
 		else
+		{
+			exit(98);
 			return (-1);
-
+		}
 		j = 0;
 		i = i + 1;
 	}
 	va_end(list);
-	print_substring(format, startpos, (i - 1));
-	return  (len + ((i - 1) - startpos) + 1);
+
+	endpos = i - 1;
+	len = len + (endpos - startpos) + 1;
+	print_substring(format, startpos, endpos);
+	return (len);
 }
 
 /**
@@ -63,12 +73,9 @@ void print_substring(const char *format, int startpos, int endpos)
 {
 	int i = startpos;
 
-	if (format)
+	while (i <= endpos)
 	{
-		while (i <= endpos)
-		{
-			write(1, format + i, 1);
-			i = i + 1;
-		}
+		write(1, format + i, 1);
+		i = i + 1;
 	}
 }
